85b355363cf6f7285834284497c7a5c1e75e7281
Mike Over <mike.over@teamsnap.com>
2017-09-14 16:38:14 -0400

    Add letters_used to tally

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -27,12 +27,13 @@ defmodule Hangman.Game do
       game_state: game.game_state,
       turns_left: game.turns_left,
       letters: game.letters |> reveal_guessed(game.used),
+      letters_used: game.used,
     }
   end
 
   ########################################################
 
-  defp accept_move(game, guess, _already_guessed = true) do
+  defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_guessed)
   end
 
@@ -62,7 +63,7 @@ defmodule Hangman.Game do
   end
 
   defp reveal_letter(letter, _in_word = true), do: letter
-  defp reveal_letter(letter, _not_in_word), do: "_"
+  defp reveal_letter(_letter, _not_in_word), do: "_"
 
   defp maybe_won(true), do: :won
   defp maybe_won(_), do: :good_guess
